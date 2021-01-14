23697c35144cbe1335899ed7536fa6aeab50f965
greinisch <greinisch@youwol.com>
2021-01-14 15:45:59 +0100

    index.ts@enh: export Stream$

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@
  * Public API Surface of flux-lib-core
  */
 
-export {attr$, child$} from "./lib/stream$"
+export {attr$, child$, Stream$} from "./lib/stream$"
 export {render, VirtualDOM} from "./lib/core"
