5b693c64fa57628c3de4bf2e6c06da582ef46cb0
greinisch <greinisch@youwol.com>
2021-04-26 11:40:09 +0200

    :art: stuffs move in interfaces file

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@
  * Public API Surface of flux-lib-core
  */
 
-export {attr$, child$, children$, Stream$} from "./lib/stream$"
-export {render, VirtualDOM, HTMLElement$} from "./lib/core"
+export {attr$, child$, Stream$} from "./lib/stream$"
+export {render, HTMLElement$} from "./lib/core"
+export {VirtualDOM} from "./lib/interface"
