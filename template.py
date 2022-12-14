4915f12a8c803441412096ace55666cc2a39b44a
youwol-guillaume <greinisch@youwol.com>
2022-12-14 11:20:40 +0100

    🔧 fix template.py

--- template.py
+++ template.py
@@ -27,7 +27,7 @@ template = Template(
             "rxjs-spy": "7.5.3"
         }),
     userGuide=True,
-    bundles=Bundles(mainModule=MainModule(entryFile="index.ts", loadDependencies=list({"rxjs": "^6.5.5"}))))
+    bundles=Bundles(mainModule=MainModule(entryFile="index.ts", loadDependencies=["rxjs"])))
 
 generate_template(template)
 shutil.copyfile(
