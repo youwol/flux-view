b1adfdb0ff4c06e7a6f1cfde40d219b635c2a5d4
youwol-guillaume <greinisch@youwol.com>
2023-02-27 17:15:15 +0100

    ♻️ index.ts => add missing symbols to export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-export { attr$, child$, Stream$, children$ } from './lib/stream$'
 export {
+    attr$,
+    child$,
+    Stream$,
+    children$,
+    AttrOption,
+    AttributeType,
+    ChildOption,
+    ChildrenOption,
+} from './lib/stream$'
+export {
+    ChildrenStream$,
     childrenAppendOnly$,
     childrenWithReplace$,
+    childrenFromStore$,
     RenderingUpdate,
+    ChildrenUpdateTrait,
+    ComparisonTrait,
+    OrderingTrait,
+    RefElement,
+    ChildrenAppendOnlyOption,
+    ChildrenFromStoreOption,
 } from './lib/advanced-children$'
 export { render, HTMLElement$ } from './lib/core'
 export { VirtualDOM } from './lib/interface'
