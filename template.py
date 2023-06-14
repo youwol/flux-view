fc955fbe31a9d3093fb7a496d32d84239a5e8d2e
youwol-guillaume <greinisch@youwol.com>
2023-06-14 07:17:14 +0200

    🔧 sync config. files w/ py-youwol module organization & pipeline TS

--- template.py
+++ template.py
@@ -1,47 +1,59 @@
 import shutil
 from pathlib import Path
 
-from youwol.pipelines.pipeline_typescript_weback_npm import Template, PackageType, Dependencies, \
-    RunTimeDeps, generate_template, Bundles, MainModule
-from youwol_utils import parse_json
+from youwol.pipelines.pipeline_typescript_weback_npm import (
+    Template,
+    PackageType,
+    Dependencies,
+    RunTimeDeps,
+    generate_template,
+    Bundles,
+    MainModule,
+)
+from youwol.utils import parse_json
 
 folder_path = Path(__file__).parent
 
-pkg_json = parse_json(folder_path / 'package.json')
+pkg_json = parse_json(folder_path / "package.json")
 
 
 template = Template(
     path=folder_path,
     type=PackageType.Library,
-    name=pkg_json['name'],
-    version=pkg_json['version'],
-    shortDescription=pkg_json['description'],
-    author=pkg_json['author'],
+    name=pkg_json["name"],
+    version=pkg_json["version"],
+    shortDescription=pkg_json["description"],
+    author=pkg_json["author"],
     dependencies=Dependencies(
         runTime=RunTimeDeps(
-            externals={
-                "rxjs": "^6.5.5"
-            },
+            externals={"rxjs": "^6.5.5"},
             includedInBundle={
                 # polyfill for WebKt based browsers (e.g. Safari)
                 # see https://github.com/WebKit/standards-positions/issues/97
                 "@ungap/custom-elements": "1.2.0"
-            }
+            },
         ),
-        devTime={
-            "rxjs-spy": "7.5.3"
-        }),
+        devTime={"rxjs-spy": "7.5.3"},
+    ),
     userGuide=True,
-    bundles=Bundles(mainModule=MainModule(entryFile="index.ts", loadDependencies=["rxjs"])))
+    bundles=Bundles(
+        mainModule=MainModule(entryFile="index.ts", loadDependencies=["rxjs"])
+    ),
+)
 
 generate_template(template)
 shutil.copyfile(
-    src=folder_path / '.template' / 'src' / 'auto-generated.ts',
-    dst=folder_path / 'src' / 'auto-generated.ts'
+    src=folder_path / ".template" / "src" / "auto-generated.ts",
+    dst=folder_path / "src" / "auto-generated.ts",
 )
-for file in ['README.md', '.gitignore', '.npmignore', '.prettierignore', 'LICENSE', 'package.json',
-             'tsconfig.json', 'webpack.config.ts']:
-    shutil.copyfile(
-        src=folder_path / '.template' / file,
-        dst=folder_path / file
-    )
+for file in [
+    "README.md",
+    ".gitignore",
+    ".npmignore",
+    ".prettierignore",
+    "LICENSE",
+    "package.json",
+    "tsconfig.json",
+    "webpack.config.ts",
+]:
+    shutil.copyfile(src=folder_path / ".template" / file, dst=folder_path / file)
